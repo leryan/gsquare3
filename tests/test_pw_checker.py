1dba3439e2d356327800cd849f531d401466bca4
Ryan Le <ryan.le@hotmail.co.uk>
2022-11-11 11:23:57 +0000

    classes challenge

---
+++ tests/test_pw_checker.py
@@ -0,0 +1,22 @@
+import pytest
+from lib.pw_checker import PasswordChecker
+
+"""
+Password incorrect length
+"""
+
+def test_bad_pw():
+    checker = PasswordChecker()
+    with pytest.raises(Exception) as e:
+        checker.check("Hello")
+    error = str(e.value)
+    assert error == "Invalid password, must be 8+ characters."
+
+"""
+Password correct length
+"""
+
+def test_good_pw():
+    checker = PasswordChecker()
+    result = checker.check("password")
+    assert result == True
